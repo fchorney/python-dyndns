4f541a5d930f8f0f6857fcf8ea4d2c1f72d4d10a
Fernando Chorney <github@djsbx.com>
2013-01-02 09:51:37 -0600

    The current date will now be printed on first message only.

--- libdyndns/Output.py
+++ libdyndns/Output.py
@@ -8,9 +8,25 @@ LEVELS = {
     'info' : 1
 }
 
+def print_date():
+    if not hasattr(print_date, 'has_been_called'):
+        from datetime import datetime
+        date = datetime.now().strftime("%A %B %d, %Y %I:%M:%S.%f %p")
+        print >> stderr, date
+        setattr(print_date, 'has_been_called', True)
+
+
 def error(msg):
     """Print msg to stderr"""
+    print_date()
     print >> stderr, 'Error: %s' % msg
+
+
+def message(msg):
+    """Always print msg to stdout"""
+    print_date()
+    print >> stdout, msg
+
 
 def exit(return_code=0, msg=None):
     """Print msg to stderr and exit with return_code"""
@@ -18,19 +34,18 @@ def exit(return_code=0, msg=None):
         error(msg)
     sysexit(return_code)
 
-def message(msg):
-    """Always print msg to stdout"""
-    print >> stdout, msg
 
 def extra(verbosity, msg):
     """Only print msg if verbosity is level extra or higher"""
     if verbosity >= LEVELS['extra']:
         message(msg)
 
+
 def debug(verbosity, msg):
     """Only print msg if verbosity is level debug or higher"""
     if verbosity >= LEVELS['debug']:
         message(msg)
+
 
 def info(verbosity, msg):
     """Only print msg if verbosity is level info or higher"""
